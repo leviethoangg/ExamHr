5c08aaa0a38c88d497b035970f725622d68a3d2b
leviethoangg <111514749+leviethoangg@users.noreply.github.com>
2024-04-03 16:52:28 +0700

    Add project files.

---
+++ Data/ExamHrContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ExamHr.Models;
+
+namespace ExamHr.Data
+{
+    public class ExamHrContext : DbContext
+    {
+        public ExamHrContext (DbContextOptions<ExamHrContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ExamHr.Models.Employee> Employee { get; set; } = default!;
+    }
+}
